f6e932a97ab1b9031c10e472b6958c8ae519d35a
Evers D MBINI <eversdarrellmbini28@gmail.com>
2025-08-15 23:13:57 +0100

    correction du requirements

--- employe/urls.py
+++ employe/urls.py
@@ -5,12 +5,12 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.liste_employes, name='liste_employes'),  # URL pour la liste des employés  
+    path('liste des employés/', views.liste_employes, name='liste_employes'),  # URL pour la liste des employés  
     path('inscription/', views.inscription_employe_drh, name='inscription_employe_drh'), 
     path('ajouter/', views.ajouter_employe, name='ajouter_employe'),   # URL pour ajouter un employé
     path('modifier/<int:id>/', views.modifier_employe, name='modifier_employe'),  # URL pour modifier un employé
     path('supprimer/<int:id>/', views.supprimer_employe, name='supprimer_employe'),  # URL pour supprimer un employé
-    path('login/', views.login_page, name='login'),
+    path('', views.login_page, name='login'),
     path('Gestploided/token/', views.LoginView.as_view(), name='token_obtain_pair'),
     path('apGestploided/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
